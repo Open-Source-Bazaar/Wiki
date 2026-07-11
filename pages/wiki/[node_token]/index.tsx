0f08d404ffb715d675283e9d8eef7362f7b01b29
South Drifter <shiy2008@gmail.com>
2026-07-12 07:08:47 +0800

    Initial commit

---
+++ pages/wiki/[node_token]/index.tsx
@@ -0,0 +1,68 @@
+import { Icon } from 'idea-react';
+import { Block, renderBlocks, WikiNode } from 'mobx-lark';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { FC } from 'react';
+import { Button, Container } from 'react-bootstrap';
+import { Minute, Second } from 'web-utility';
+
+import { PageHead } from '../../../components/Layout/PageHead';
+import documentStore from '../../../models/Document';
+import wikiStore from '../../../models/Wiki';
+import { lark } from '../../api/Lark/core';
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  await lark.getAccessToken();
+
+  const nodes = await wikiStore.getAll();
+
+  return {
+    paths: nodes.map(({ node_token }) => ({ params: { node_token } })),
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  await lark.getAccessToken();
+
+  const node = await wikiStore.getOne(params!.node_token as string);
+
+  if (node?.obj_type !== 'docx') return { notFound: true };
+
+  try {
+    const blocks = await documentStore.getOneBlocks(
+      node.obj_token,
+      token => `/api/Lark/file/${token}/placeholder`,
+    );
+
+    return { props: { node, blocks } };
+  } catch (error) {
+    console.error(error);
+
+    return { notFound: true, revalidate: Minute / Second };
+  }
+};
+
+interface WikiDocumentPageProps {
+  node: WikiNode;
+  blocks: Block<any, any, any>[];
+}
+
+const WikiDocumentPage: FC<WikiDocumentPageProps> = ({ node, blocks }) => (
+  <Container>
+    <PageHead title={node.title} />
+
+    <aside className="text-end">
+      <Button
+        variant="outline-primary"
+        size="sm"
+        href={`/wiki/${node.node_token}/debugger`}
+      >
+        <Icon name="tools" />
+      </Button>
+    </aside>
+
+    {renderBlocks(blocks)}
+  </Container>
+);
+
+export default WikiDocumentPage;
